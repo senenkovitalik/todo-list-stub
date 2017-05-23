7ac75f941e148aefb6887bf479976af6b5956649
Vitaliy Senenko <senenkovitalik@gmail.com>
2017-05-23 14:07:27 +0300

    Finish DAO(LocalStorage)

--- js/dao/TaskLocalStorage.js
+++ js/dao/TaskLocalStorage.js
@@ -5,10 +5,10 @@ AppScope.TaskLocalStorage = (function(){
     var TASKS_KEY = AppScope.localStorageConstants.TASK_LIST;
     var Task = AppScope.Task;
 
-    // get all tasks from LS
-    function getAll() {
+    // get all tasks
+    function getAll(){
         try {
-            var taskListStringified = localStorage.TASKS_KEY.trim(),
+            var taskListStringified = localStorage.getItem(TASKS_KEY).trim(),
                 taskList = JSON.parse(taskListStringified);
 
             var list = [];
@@ -19,7 +19,6 @@ AppScope.TaskLocalStorage = (function(){
                 var task = new Task();
                 var taskObj = task.fromJSON(taskList[i]);
                 list.push(taskObj);
-
             }
 
             return list;
@@ -28,7 +27,7 @@ AppScope.TaskLocalStorage = (function(){
         }
     }
 
-    // save all tasks to LS
+    // save all tasks
     function saveAll(taskList){
         var arr = [];
         for (var i = 0; i < taskList.length; i++) {
@@ -37,16 +36,34 @@ AppScope.TaskLocalStorage = (function(){
         localStorage.setItem(TASKS_KEY, JSON.stringify(arr));
     }
 
-    // save one task to LS
-    function saveTask(task) {
+    // save one task
+    function saveTask(task){
         var taskList = getAll();
         taskList.push(task);
         saveAll(taskList);
     }
 
+    // remove one task
+    function removeTask(task){
+        var taskList = getAll();
+        for (var i = 0; i < taskList.length; i++) {
+            if (task.id === taskList[i].id) {
+                taskList.slice(i, 1);
+            }
+        }
+        saveAll(taskList);
+    }
+
+    // remove all tasks
+    function removeAll(){
+        localStorage.setItem(TASKS_KEY, []);
+    }
+
     return {
         getAll: getAll,
         saveAll: saveAll,
-        saveTask: saveTask
+        saveTask: saveTask,
+        removeTask: removeTask,
+        removeAll: removeAll
     }
 })();
